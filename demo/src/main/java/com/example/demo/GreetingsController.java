1a1a72564970e121fb72b4e7c570afb8e3fdf2cf
Cleber Fadil <cleber.fadil@gmail.com>
2025-03-26 22:09:26 -0300

    in memory authentication - multi user with roles and role basewd authorization

--- demo/src/main/java/com/example/demo/GreetingsController.java
+++ demo/src/main/java/com/example/demo/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String SayHello() {
         return "Hello, World!";
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @GetMapping("/user")    
+    public String UserEndpoint() {
+        return "Hello, User";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "Hello, Admin";
     }
 }
